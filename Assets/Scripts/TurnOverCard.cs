c0e3984ec6c36f95ac64ddedd47a88ffba6cef1a
lbte93 <l.bte93@gmail.com>
2022-02-11 17:39:17 +0000

    Intento de arreglar la carta. Fail.

--- Assets/Scripts/TurnOverCard.cs
+++ Assets/Scripts/TurnOverCard.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class TurnOverCard : MonoBehaviour, IPointerDownHandler
+public class TurnOverCard : MonoBehaviour
 {
     public GameObject cardBack;
 
@@ -11,9 +11,10 @@ public class TurnOverCard : MonoBehaviour, IPointerDownHandler
 
     void Update()
     {
-        
+
         /*if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Mouse clicked");
             // convert the click position to world space in order to properly compare against the position of the GameObjects.
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             // to drop the z-coordinate, and use it as the starting point. 
@@ -23,18 +24,67 @@ public class TurnOverCard : MonoBehaviour, IPointerDownHandler
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 
             // to determine if anything was hit by the click
-            if (hit.collider != null)
+            Debug.Log("You selected the " + hit.collider.gameObject.name);
+            if (hit.collider.gameObject.name == "ProjectCardFront" || hit.collider.gameObject.name == "ProjectCardBack")
             {
-                Debug.Log(hit.collider.gameObject.name);
-                hit.collider.attachedRigidbody.AddForce(Vector2.up);
+                Debug.Log("You selected the " + hit.collider.gameObject.name);
+                //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+                StartFlip();
             }
-            StartFlip();
         }*/
-    }
 
-    public void OnPointerDown(PointerEventData event_data)
-    {
-        StartFlip();
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            // turning a screen point into a ray
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                
+
+                if (hit.transform != null)
+                {
+                    Debug.Log("You selected the " + hit.transform.gameObject.name);
+                }
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+                /*if (hit.transform.gameObject.name == "ProjectCardFront" || hit.transform.gameObject.name == "ProjectCardBack")
+                {
+                    Debug.Log("You selected the " + hit.transform.gameObject.name);
+                    //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+                    StartFlip();
+                }*/
+            }
+        }
+
+        /*if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is pressed down");
+            Camera cam = Camera.main;
+
+            //Raycast depends on camera projection mode
+            Vector2 origin = Vector2.zero;
+            Vector2 dir = Vector2.zero;
+
+            if (cam.orthographic)
+            {
+                origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+            else
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                origin = ray.origin;
+                dir = ray.direction;
+            }
+
+            RaycastHit2D hit = Physics2D.Raycast(origin, dir);
+
+            //Check if we hit anything
+            if (hit)
+            {
+                Debug.Log("We hit " + hit.collider.name);
+            }
+        }*/
     }
 
     public void StartFlip()
