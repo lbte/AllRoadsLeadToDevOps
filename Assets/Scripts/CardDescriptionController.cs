a3f234fe8be6667470363c3f63006c3824d85192
lbte93 <l.bte93@gmail.com>
2022-02-12 17:23:23 +0000

    Error en CardDescriptionController, Object reference not set to an instance of an object. No sé por qué.

--- Assets/Scripts/CardDescriptionController.cs
+++ Assets/Scripts/CardDescriptionController.cs
@@ -10,14 +10,19 @@ public class CardDescriptionController : MonoBehaviour
     public Image description_window;
     public Text text_description_window;
     public Animator animator_description_window;
+    private Image plan_tools_card_image;
 
     private GameObject plan_stage;
-    //private GameObject plan_project;
+    private GameObject plan_project;
+
     private GameObject plan_architecture;
     private PlanCarouselController plan_carousel_script;
     private PlanDeckController plan_deck_script;
     private GameObject plan_abilities;
-    //private GameObject plan_tools;
+
+    private GameObject plan_tools;
+    private Card random_card;
+
     private StageController stage_controller_script;
     private GameObject code_stage;
 
@@ -25,21 +30,14 @@ public class CardDescriptionController : MonoBehaviour
     private GameObject build_stage;
     private BuildCarouselController build_carousel_script;
 
+    private SingleCardController single_card_script;
+
     void Start()
     {
         // Assignment of each game object
         description_button = GameObject.Find("CardDescriptionButton").GetComponent<Button>();
         close_description_button = GameObject.Find("CardDescriptionCloseButton").GetComponent<Button>();
-
-        plan_stage = GameObject.Find("Plan");
-        //plan_project = GameObject.Find("PlanProject").GetComponent<GameObject>();
-        plan_architecture = GameObject.Find("PlanArchitectures");
-        plan_abilities = GameObject.Find("PlanAbilities");
-        //plan_tools = GameObject.Find("PlanTools").GetComponent<GameObject>();
-        stage_controller_script = GameObject.Find("Views").GetComponent<StageController>();
-        code_stage = GameObject.Find("CodeItems");
-        build_stage = GameObject.Find("BuildItems");
-
+        plan_tools_card_image = GameObject.Find("ToolCardImage").GetComponent<Image>();
         description_button.onClick.AddListener(DescriptionWindow);
         close_description_button.onClick.AddListener(CloseDescriptionWindow);
     }
@@ -51,38 +49,50 @@ public class CardDescriptionController : MonoBehaviour
 
     void StageDescriptionAssignment()
     {
+        stage_controller_script = GameObject.Find("Views").GetComponent<StageController>();
         // CREO QUE ESTO TAL VEZ SE PODRÍA REALIZAR CON LOS SCRIPTS YA EXISTENTES, PERO NO QUIERO PENSAR. ASÍ ESTÁ FUNCIONANDO, SO NO SÉ. USTEDES DIRÁN.
-        if(stage_controller_script.stage_title_text.text == "PLAN")
+        if (stage_controller_script.stage_title_text.text == "PLAN") // ERROR: NULL REFERENCE NOT SET TO AN INSTANCE OF AN OBJECT
         {
+            plan_stage = GameObject.Find("Plan");
             // Depending on which deck we are in, assign the description of the center card accordingly
             plan_deck_script = plan_stage.GetComponent<PlanDeckController>();
 
             if (plan_deck_script.deck_button_text.text == "Project")
             {
-                text_description_window.text = "PROJEEECT"; //TODO: ACCEDER AL ATRIBUTO CARD_DESCRIPTION PARA ESTA CARTA QUE ESTÁ SOLA.
+                plan_project = GameObject.Find("PlanProject");
+                single_card_script = plan_project.GetComponent<SingleCardController>();
+                text_description_window.text = single_card_script.cards[0].card_description; // ERROR: Object reference not set to an instance of an object. NO VEO LA RAZÓN
             }
             else if (plan_deck_script.deck_button_text.text == "Architecture")
             {
+                plan_architecture = GameObject.Find("PlanArchitectures");
                 plan_carousel_script = plan_architecture.GetComponent<PlanCarouselController>();
                 text_description_window.text = plan_carousel_script.cards[plan_carousel_script.center_index].card_description;
             }
             else if (plan_deck_script.deck_button_text.text == "Abilities")
             {
+                plan_abilities = GameObject.Find("PlanAbilities");
                 plan_carousel_script = plan_abilities.GetComponent<PlanCarouselController>();
                 text_description_window.text = plan_carousel_script.cards[plan_carousel_script.center_index].card_description;
             }
             else if (plan_deck_script.deck_button_text.text == "Tools")
             {
-                text_description_window.text = ""; //TODO: ACCEDER AL ATRIBUTO CARD_DESCRIPTION PARA ESTA CARTA QUE ESTÁ SOLA.
+                plan_tools = GameObject.Find("PlanTools");
+                single_card_script = plan_tools.GetComponent<SingleCardController>();
+                random_card = single_card_script.cards[Random.Range(0, single_card_script.cards.Count)];
+                plan_tools_card_image.sprite = random_card.card_image;
+                text_description_window.text = random_card.card_description; 
             }
         }
         else if (stage_controller_script.stage_title_text.text == "CODE")
         {
+            code_stage = GameObject.Find("CodeItems");
             code_carousel_script = code_stage.GetComponent<CodeCarouselController>();
             text_description_window.text = code_carousel_script.deck[code_carousel_script.center_index].card_description;
         }
         else if (stage_controller_script.stage_title_text.text == "BUILD")
         {
+            build_stage = GameObject.Find("BuildItems");
             build_carousel_script = build_stage.GetComponent<BuildCarouselController>();
             text_description_window.text = build_carousel_script.cards[build_carousel_script.center_index].card_description;
         }
---
+++ Assets/Scripts/SingleCardController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleCardController : MonoBehaviour
+{ 
+    public List<Card> cards = new List<Card>();
+    
+}
